2c4bd406db16e1142f3c93c4eea8851d4b748f9e
Starmism <minecraftpros1225@gmail.com>
2020-04-02 19:46:36 -0600

    Cleaned up everything, removed Main from like, everything, renamed a few things

--- src/me/gt3ch1/betterlife/Main/Main.java
+++ src/me/gt3ch1/betterlife/Main/Main.java
@@ -1,13 +1,11 @@
 package me.gt3ch1.betterlife.Main;
 
-import me.gt3ch1.betterlife.configuration.MainConfigurationHandler;
-import me.gt3ch1.betterlife.configuration.PlayerConfigurationHandler;
+import me.gt3ch1.betterlife.commandhelpers.CommandUtils;
 import me.gt3ch1.betterlife.events.BlockFade;
 import me.gt3ch1.betterlife.events.ComposterClick;
 import me.gt3ch1.betterlife.events.PlayerJoin;
-import me.gt3ch1.betterlife.events.PlayerWalk;
-import me.gt3ch1.betterlife.tabcompletor.TabCompletorHelper;
-
+import me.gt3ch1.betterlife.events.PlayerMove;
+import me.gt3ch1.betterlife.tabcompleter.TabCompleterHelper;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -17,37 +15,33 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
 
 public class Main extends JavaPlugin {
-	private MainConfigurationHandler ch;
-	private PlayerConfigurationHandler pch;
 	private Listener blockFadeListener,playerMoveListener,playerJoinListener,composterClickListener;
+	Listener[] enabledListeners = {blockFadeListener,playerMoveListener,playerJoinListener,composterClickListener};
 	public Main m;
+
 	@Override
 	public void onEnable() {
+		// Initialize Main variable
+		m = this;
 		// Config setup
-		ch = new MainConfigurationHandler(this);
-		pch = new PlayerConfigurationHandler(this);
-		saveDefaultConfig();
-		pch.getCustomConfig();
-		pch.saveCustomConfig();
-		String[] enabledTabCommands = {"toggledownfall","trail"};
+		CommandUtils.enableConfiguration(m);
 		// Listener setup
-		blockFadeListener = new BlockFade(this);
-	 	playerMoveListener = new PlayerWalk(this);
-	 	playerJoinListener = new PlayerJoin(this);
-	 	composterClickListener = new ComposterClick(this);
+		blockFadeListener = new BlockFade();
+		composterClickListener = new ComposterClick();
+		playerMoveListener = new PlayerMove();
+		playerJoinListener = new PlayerJoin();
+/*		for (Listener listener : enabledListeners) {
+			Bukkit.getPluginManager().registerEvents(listener, this);
+		}*/
 		Bukkit.getPluginManager().registerEvents(blockFadeListener, this);
 		Bukkit.getPluginManager().registerEvents(playerMoveListener, this);
 		Bukkit.getPluginManager().registerEvents(playerJoinListener, this);
-		Bukkit.getPluginManager().registerEvents(composterClickListener,this);
-		// Initialize Main variable
-		m = this;
-		//TODO: Set tab completors.  Need a way to load all of the commands.  I've got an idea
-		// Make the superclass of BetterLifeCommands add to an array of strings in this class.
-		for (String command : enabledTabCommands) {
-			getCommand(command).setTabCompleter(new TabCompletorHelper(m));
+		Bukkit.getPluginManager().registerEvents(composterClickListener, this);
+		// Tab Completion setup
+		for (String command : CommandUtils.getEnabledTabCommands()) {
+			getCommand(command).setTabCompleter(new TabCompleterHelper());
 		}
 		// Log output
 		getLogger().info(ChatColor.GREEN + "Enabled!");
@@ -55,37 +49,28 @@ public class Main extends JavaPlugin {
 
 	@Override
 	public void onDisable() {
-		ch = null;
-		pch = null;
-	
-		blockFadeListener = null;
-		playerMoveListener = null;
-		playerMoveListener = null;
-		composterClickListener = null;
-		
+		// Set the configuration managers to null
+		CommandUtils.disableConfiguration(m);
+		// Set all listeners to null
+		for (Listener listener : enabledListeners) {
+			listener = null;
+		}
+		// Log output
 		getLogger().info(ChatColor.RED + "Disabled!");
 	}
 
 	@Override
 	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
+		// Sends the command to the designated class file
 		try {
 			this.getCommand(label).setExecutor((CommandExecutor) Class
 					.forName("me.gt3ch1.betterlife.commands." + label.toUpperCase())
-					.getConstructor(Main.class, String.class, CommandSender.class, Command.class, String.class, String[].class)
-					.newInstance(this, label.toLowerCase(), cs, cmd, label, args));
+					.getConstructor(String.class, CommandSender.class, Command.class, String.class, String[].class)
+					.newInstance(label.toLowerCase(), cs, cmd, label, args));
 		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | IllegalArgumentException
 				| InvocationTargetException | NoSuchMethodException | SecurityException e) {
 			e.printStackTrace();
 		}
 		return true;
 	}
-
-	public MainConfigurationHandler getMainConfiguration() {
-		return ch;
-	}
-
-	public PlayerConfigurationHandler getPlayerConfiguration() {
-		return pch;
-	}
-
 }
