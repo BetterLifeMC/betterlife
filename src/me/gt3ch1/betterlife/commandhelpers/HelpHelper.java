959a298602fb422edb533926c8b3ae52239f2bc2
Starmism <iamstarmism@gmail.com>
2020-04-05 01:02:49 -0600

    Overhauled help system, designed for growth

---
+++ src/me/gt3ch1/betterlife/commandhelpers/HelpHelper.java
@@ -0,0 +1,28 @@
+package me.gt3ch1.betterlife.commandhelpers;
+
+import java.util.LinkedHashMap;
+
+public class HelpHelper {
+
+    private static LinkedHashMap<String, String> trailHelpHash = new LinkedHashMap<String, String>();
+
+    public static LinkedHashMap<String, String> getAHelpHash(String hash) {
+        switch(hash) {
+            case "trail":
+                return trailHelpHash;
+        }
+        return null;
+    }
+
+    public static void setupAllHelpHashes() {
+        setupTrailHelpHash();
+    }
+
+    private static void setupTrailHelpHash() {
+        trailHelpHash.put("set <trail>", "Sets the current player's trail");
+        trailHelpHash.put("add <trail>", "Adds the trail to the config");
+        trailHelpHash.put("rm <trail>", "Removes the trail from the config");
+        trailHelpHash.put("list", "Lists the enabled trails");
+        trailHelpHash.put("toggle", "Toggles your trail");
+    }
+}
