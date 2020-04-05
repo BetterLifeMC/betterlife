022320d5c6004604be4d4045bbf82bb1d1e112df
GT3CH1 <gcpease@gt3ch1.tk>
2020-04-05 09:01:55 -0600

    Added JavaDoc/Comments on roadboost

--- src/me/gt3ch1/betterlife/commands/ROADBOOST.java
+++ src/me/gt3ch1/betterlife/commands/ROADBOOST.java
@@ -9,16 +9,31 @@ import org.bukkit.entity.Player;
 
 public class ROADBOOST extends BetterLifeCommands implements CommandExecutor {
 
+	/**
+	 * Runs the command /roadboost
+	 * @param permission
+	 * @param cs
+	 * @param c
+	 * @param label
+	 * @param args
+	 */
 	public ROADBOOST(String permission, CommandSender cs, Command c, String label, String[] args) {
 		super(permission, cs, c, label, args);
 		this.onCommand(cs, c, label, args);
 	}
 
+	/**
+	 * Toggles whether or not the player can be fast
+	 */
 	@Override
 	public boolean onCommand(CommandSender sender, Command c, String command, String[] args) {
+		// If we have a player
 		if (sender instanceof Player) {
+			// Cast it to player
 			Player p = (Player) sender;
+			// And if they have permission
 			if (p.hasPermission(this.getPermission())) {
+				// Make them FAST
 				boolean hasToggleSprintEnabled = CommandUtils.getPlayerConfiguration().getCustomConfig().getBoolean("player."+p.getUniqueId().toString()+".pathboost");
 				CommandUtils.getPlayerConfiguration().getCustomConfig().set("player."+p.getUniqueId().toString()+".pathboost", !hasToggleSprintEnabled);
 				CommandUtils.getPlayerConfiguration().saveCustomConfig();
