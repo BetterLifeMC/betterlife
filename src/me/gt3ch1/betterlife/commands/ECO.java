b93b9b3f8e610f5b77b181a7015c67cca64dadef
GT3CH1 <gcpease@gt3ch1.tk>
2020-04-17 07:24:56 -0600

    Added switch statement for /eco give <player>

--- src/me/gt3ch1/betterlife/commands/ECO.java
+++ src/me/gt3ch1/betterlife/commands/ECO.java
@@ -2,6 +2,7 @@ package me.gt3ch1.betterlife.commands;
 
 import me.gt3ch1.betterlife.commandhelpers.BetterLifeCommands;
 import me.gt3ch1.betterlife.commandhelpers.HelpHelper;
+import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -35,6 +36,31 @@ public class ECO extends BetterLifeCommands implements CommandExecutor {
                     switch (args[0]) {
                         case "bal":
                             sendBannerMessage(player, "&aBalance: " + economy.getBalance(player));
+                            return true;
+                        case "give":
+                        case "set":
+                            sendHelpMessage(sender, "eco", helpHash);
+                            return true;
+                        default:
+                            return false;
+                    }
+                case 2:
+                    switch (args[0]){
+                        case "give":
+                            Player commandReceiver = Bukkit.getPlayer(args[1]);
+                            if (commandReceiver instanceof Player && args.length==3){
+                                double playerBalance = Double.valueOf(args[2]);
+                                if((economy.getBalance(player) - playerBalance) > 0) {
+                                    economy.bankDeposit(commandReceiver.getName(), playerBalance);
+                                    economy.withdrawPlayer(commandReceiver, playerBalance);
+                                    sendBannerMessage(player, "&aYou sent &e" + playerBalance +"&a to " + commandReceiver.getName());
+                                    return true;
+                                }
+                                return false;
+                            }
+                            return false;
+                        case "set":
+                            sendHelpMessage(sender, "eco", helpHash);
                             return true;
                         default:
                             return false;
