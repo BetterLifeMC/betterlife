2e4288f41d946790f82536d032f7f52f3fb0c618
GT3CH1 <gcpease@gt3ch1.tk>
2020-04-19 18:20:42 -0600

    Finished the anti tnt-griefing

--- src/me/gt3ch1/betterlife/events/BlockExplode.java
+++ src/me/gt3ch1/betterlife/events/BlockExplode.java
@@ -5,26 +5,20 @@ import me.gt3ch1.betterlife.configuration.PlayerConfigurationHandler;
 import me.gt3ch1.betterlife.eventhelpers.BlockBreakHelper;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
-
-import java.util.Arrays;
 
 public class BlockExplode implements Listener {
     Object[] playerUUIDS;
     private static BlockBreakHelper bbh = new BlockBreakHelper();
-
     @EventHandler
     public void antiGriefBlockExplode(EntityExplodeEvent e) {
         PlayerConfigurationHandler playerConfigs = CommandUtils.getPlayerConfiguration();
         playerUUIDS = playerConfigs.getCustomConfig().getConfigurationSection("player").getKeys(false).toArray();
         try {
-            for (Block b : e.blockList()) {
+            for (Block b : e.blockList().toArray(new Block[e.blockList().size()])) {
 
                 Location location = b.getLocation();
                 double locationX = location.getX();
@@ -46,8 +40,9 @@ public class BlockExplode implements Listener {
                         Bukkit.getLogger().info(String.format("x1 %s | x2 %s :: z1 %s | z2 %s \n is it cancelled? → %s",x1,x2,z1,z2,bbh.isWithinClaimedArea(x1, x2, z1, z2, locationX, locationZ)));
 
                         if (bbh.isWithinClaimedArea(x1, x2, z1, z2, locationX, locationZ)) {
-                            Bukkit.getLogger().info(String.format("Cancelling event at : %s : %s ", locationX, locationZ));
+                            e.blockList().clear();
                             e.setCancelled(true);
+                            Bukkit.getLogger().info(String.format("Cancelling event at : %s : %s ", locationX, locationZ));
                         }
                     }
                 }
