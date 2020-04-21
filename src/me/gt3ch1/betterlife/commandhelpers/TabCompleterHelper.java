1e0aba5ac49a0de5f754ccb7e33fe83f02f4b754
GT3CH1 <gcpease@gt3ch1.tk>
2020-04-21 16:23:58 -0600

    Fixed issue with trial set

--- src/me/gt3ch1/betterlife/commandhelpers/TabCompleterHelper.java
+++ src/me/gt3ch1/betterlife/commandhelpers/TabCompleterHelper.java
@@ -9,13 +9,11 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * Handles the ability of tab completion for commands as designated in CommandUtils.
+ *
  * @author gt3ch1
  * @author Starmism
  */
@@ -23,7 +21,8 @@ public class TabCompleterHelper implements TabCompleter {
     // Initialize a string of variables
     private List<String> subCommands = new ArrayList<>();
     private List<String> newList = new ArrayList<>();
-    private Particle[] particles = Particle.values();
+    private final Particle[] particles = Particle.values();
+    private final List<String> enabledParticles = CommandUtils.getParticleConfiguration().getParticleConfig().getStringList("enabledParticles");
 
     /**
      * When we can tab complete commands.
@@ -43,37 +42,31 @@ public class TabCompleterHelper implements TabCompleter {
                         case 1:
                             // Add set, list, help, toggle, and if the player
                             // has permissions, add and rm to the tab completion.
-                            if (Arrays.stream(args).anyMatch("set"::contains)) {
+                            if (Arrays.stream(args).anyMatch("set"::contains))
                                 subCommands.add("set");
-                                System.out.println("Is set?!");
-                                System.out.println(subCommands.toString());
-                            }
-                            if (Arrays.stream(args).anyMatch("list"::contains)) {
+
+                            if (Arrays.stream(args).anyMatch("list"::contains))
                                 subCommands.add("list");
-                            }
-                            if (Arrays.stream(args).anyMatch("help"::contains)) {
+
+                            if (Arrays.stream(args).anyMatch("help"::contains))
                                 subCommands.add("help");
-                            }
-                            if (Arrays.stream(args).anyMatch("toggle"::contains)) {
+
+                            if (Arrays.stream(args).anyMatch("toggle"::contains))
                                 subCommands.add("toggle");
-                            }
+
                         case 2:
-                            switch (args[0]) {
-                                // If it is set, return the list of currently enabled particles,
-                                // and append them to the tablist list.
-                                case "set":
-                                    subCommands = (List<String>) (CommandUtils.getParticleConfiguration().getParticleConfig().getList("enabledParticles"));
-                                    for (int i = 0; i < subCommands.size(); i++) {
-                                        String particle = subCommands.get(i);
-                                        if (Arrays.stream(args).anyMatch(particle::contains) && player
-                                                .hasPermission("betterlife.trail.particle." + particle.toLowerCase())) {
-                                            newList.add(particle);
-                                        }
-                                    }
-                                    subCommands = newList;
-                                    break;
-                                default:
-                                    break;
+                            // If it is set, return the list of currently enabled particles,
+                            // and append them to the tablist list.
+                            if ("set".equals(args[0])) {
+                                for (int i =0; i < enabledParticles.size(); i++) {
+
+                                    String particle = enabledParticles.get(i);
+                                    if (Arrays.stream(args).anyMatch(particle::contains) && player
+                                            .hasPermission("betterlife.trail.particle." + particle.toLowerCase()))
+                                        newList.add(particle);
+
+                                }
+                                subCommands = newList;
                             }
                         default:
                             break;
@@ -90,19 +83,15 @@ public class TabCompleterHelper implements TabCompleter {
                     }
                     break;
                 case "bl":
-                    switch (args.length) {
-                        case 1:
-                            if (player.hasPermission("betterlife.reload") || player.hasPermission("betterlife.admin")) {
-                                if (Arrays.stream(args).anyMatch("reload"::contains)) {
-                                    subCommands.add("reload");
-                                }
+                    if (args.length == 1) {
+                        if (player.hasPermission("betterlife.reload") || player.hasPermission("betterlife.admin")) {
+                            if (Arrays.stream(args).anyMatch("reload"::contains)) {
+                                subCommands.add("reload");
                             }
-                            if (Arrays.stream(args).anyMatch("version"::contains)) {
-                                subCommands.add("version");
-                            }
-                            break;
-                        default:
-                            break;
+                        }
+                        if (Arrays.stream(args).anyMatch("version"::contains)) {
+                            subCommands.add("version");
+                        }
                     }
                     break;
                 case "eco":
@@ -119,16 +108,14 @@ public class TabCompleterHelper implements TabCompleter {
                                     subCommands.add("set");
                             break;
                         case 2:
-                            switch (args[0]) {
-                                case "bal":
-                                    if (player.hasPermission("betterlife.eco.bal")) {
-                                        for (Player p : Bukkit.getServer().getOnlinePlayers()) {
-                                            if (Arrays.stream(args).anyMatch(p.getName().toString()::contains))
-                                                subCommands.add(p.getName().toString());
+                            if ("bal".equals(args[0])) {
+                                if (player.hasPermission("betterlife.eco.bal")) {
+                                    for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+                                        if (Arrays.stream(args).anyMatch(p.getName()::contains))
+                                            subCommands.add(p.getName());
 
-                                        }
                                     }
-
+                                }
                             }
                             break;
                         default:
