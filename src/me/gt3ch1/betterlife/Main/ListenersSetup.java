63f7c9db9c98bb3f13effb827b202b3d256419e6
GT3CH1 <gcpease@gt3ch1.tk>
2020-04-20 21:36:57 -0600

    Added BlockIgnite listener

--- src/me/gt3ch1/betterlife/Main/ListenersSetup.java
+++ src/me/gt3ch1/betterlife/Main/ListenersSetup.java
@@ -4,25 +4,30 @@ import me.gt3ch1.betterlife.events.*;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 
-import java.util.ArrayList;
-
+/**
+ * Enabled the listeners needed for BetterLife to run properly.
+ * @author gt3ch1
+ */
 public class ListenersSetup {
 
-    Main plugin;
 
-    public ListenersSetup(Main m){
-        this.plugin=m;
+    /**
+     * Enables the needed listeners for BetterLife.
+     * @param plugin
+     */
+    public ListenersSetup(Main plugin){
+
         plugin.listeners.add(new BlockFade());
         plugin.listeners.add(new PlayerMove());
         plugin.listeners.add(new PlayerJoin());
         plugin.listeners.add(new PlayerInteract());
         plugin.listeners.add(new BlockBreak());
         plugin.listeners.add(new BlockExplode());
-
+        plugin.listeners.add(new BlockIgnite());
 
 		for (Listener listener : plugin.listeners) {
-            Bukkit.getPluginManager().registerEvents(listener, m);
-            plugin.getLogger().info("Enabling listener: " + listener);
+            Bukkit.getPluginManager().registerEvents(listener, plugin);
+            plugin.getLogger().info("Enabling listener: " + listener.toString());
         }
     }
 }
