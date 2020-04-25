a9c75da524ec59927544aea4aa8bf17239a8003f
GT3CH1 <gcpease@gt3ch1.tk>
2020-04-25 14:58:30 -0600

    Make SQL more doer

--- src/me/gt3ch1/betterlife/events/PlayerJoin.java
+++ src/me/gt3ch1/betterlife/events/PlayerJoin.java
@@ -1,39 +1,37 @@
 package me.gt3ch1.betterlife.events;
 
-import me.gt3ch1.betterlife.Main.Main;
 import me.gt3ch1.betterlife.commandhelpers.CommandUtils;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
+import me.gt3ch1.betterlife.configuration.PlayerConfigurationHandler;
 import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
-import java.sql.SQLException;
+import java.util.UUID;
 
 public class PlayerJoin implements Listener {
-
+    PlayerConfigurationHandler playerConfig  = CommandUtils.getPlayerConfiguration();
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
 
         Player p = e.getPlayer();
         Particle newPlayerParticle = Particle.valueOf(CommandUtils.getMainConfiguration().getCustomConfig().getString("defaultParticle").toUpperCase());
-
-        try {
-
-            newPlayerParticle = Particle.valueOf(CommandUtils.getPlayerConfiguration().getCustomConfig().getString("player." + p.getUniqueId().toString() + ".trail"));
-
-        } catch (Exception ex) {
-
-            try {
-                CommandUtils.getPlayerConfiguration().setValue("trails.trail",newPlayerParticle.toString(),p.getUniqueId().toString());
-                CommandUtils.getPlayerConfiguration().setValue("trails.enabled",false,p.getUniqueId().toString());
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-
-
+        
+        try{
+            playerConfig.getStringValue("trails.enabled", p.getUniqueId());
+        }catch (Exception ex){
+            playerConfig.setValue("trails.trail", newPlayerParticle.toString(), p.getUniqueId());
+            playerConfig.setValue("trails.enabled", false, p.getUniqueId());
         }
+        playerConfig.trailEnabledPerPlayer.put(p.getUniqueId(),playerConfig.getBooleanValue("trails.enabled",p.getUniqueId()));
+        playerConfig.trailPerPlayer.put(p.getUniqueId(),playerConfig.getStringValue("trails.trail",p.getUniqueId()));
+    }
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent e){
+        UUID uuid = e.getPlayer().getUniqueId();
+        playerConfig.trailEnabledPerPlayer.remove(uuid);
+        playerConfig.trailPerPlayer.remove(uuid);
     }
 }
