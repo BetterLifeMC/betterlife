815118f9a6dca1b33f0e33f275a6dfe68d21cbe1
GT3CH1 <gcpease@gt3ch1.tk>
2020-04-26 10:50:39 -0600

    Log SQL queries (for now), added method executeUpdate to simplify calls.

--- src/me/gt3ch1/betterlife/sql/Sql.java
+++ src/me/gt3ch1/betterlife/sql/Sql.java
@@ -18,20 +18,25 @@ public class Sql {
         this.username = username;
         this.password = password;
         this.server = server;
-        Bukkit.getLogger().info(ChatColor.RED + "Running SQL...");
+        doBukkitLog(ChatColor.RED + "Running SQL...");
         connect();
     }
-
+    public void doBukkitLog(String log){
+        Bukkit.getLogger().info(log);
+    }
     private void connect() {
         try {
+
             con = DriverManager.getConnection("jdbc:mysql://" + server + ":3306/" + database,
                     username, password);
             stmt = con.createStatement();
             rs = stmt.executeQuery("SELECT * FROM betterlife_players");
-            Bukkit.getLogger().info(ChatColor.BLUE + "SQL Succeeded...");
+            doBukkitLog(ChatColor.BLUE + "SQL Succeeded...");
+
         } catch (SQLException throwables) {
+
             throwables.printStackTrace();
-            Bukkit.getLogger().info(ChatColor.RED + "SQL Failed!...");
+            doBukkitLog(ChatColor.RED + "SQL Failed!...");
 
         }
     }
@@ -40,57 +45,80 @@ public class Sql {
         return stmt;
     }
 
-    public void executeQuery(String query) throws SQLException {
-        getStatement().executeQuery(query);
+    public ResultSet executeQuery(String query) {
+        try {
+            doBukkitLog(ChatColor.LIGHT_PURPLE + query);
+            return getStatement().executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public int executeUpdate(String query) {
+        try {
+            doBukkitLog(ChatColor.LIGHT_PURPLE + query);
+            return getStatement().executeUpdate(query);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return -1;
     }
 
     public Object getValue(String row, String playerUUID) {
 
         row = row.replace(".", "_");
         playerUUID = playerUUID.replace("-", "_");
+        String query = "SELECT `" + row + "` FROM `betterlife_players` WHERE `uuid` = '" + playerUUID + "'";
+
         try {
-            rs = getStatement().executeQuery("SELECT `" + row + "` FROM `betterlife_players` WHERE `uuid` = '" + playerUUID + "'");
+            rs = executeQuery(query);
             while (rs.next())
                 return rs.getObject(row);
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+
         return null;
     }
 
     public List<String> getRows(String row) {
+
         List<String> playerUUIDs = new ArrayList<>();
         try {
-            rs = getStatement().executeQuery("SELECT `" + row + "` FROM `betterlife_players");
+            rs = executeQuery("SELECT `" + row + "` FROM `betterlife_players");
             while (rs.next()) {
                 playerUUIDs.add(rs.getString(row).replace("_", "-"));
             }
             return playerUUIDs;
         } catch (SQLException throwables) {
+
             throwables.printStackTrace();
+
         }
         return null;
     }
 
     public void setValue(String row, Object value, String playerUUID) throws SQLException {
+
         row = row.replace(".", "_");
         playerUUID = playerUUID.replace("-", "_");
-        System.out.println(String.format("Row » %s || Player UUID » %s || Value » %s", row, playerUUID, value));
-        rs = getStatement().executeQuery("SELECT * FROM `betterlife_players` WHERE `uuid`= '" + playerUUID + "'");
+        rs = executeQuery("SELECT * FROM `betterlife_players` WHERE `uuid`= '" + playerUUID + "'");
         String statement = "";
+
         if (rs.next()) {
             String sql = "UPDATE `Minecraft`.`betterlife_players` SET `" + row + "` = '"
                     + value + "' WHERE `uuid` = '" + playerUUID + "'";
-            Bukkit.getLogger().info(ChatColor.LIGHT_PURPLE + sql);
+            doBukkitLog(ChatColor.LIGHT_PURPLE + sql);
 
             getStatement().executeUpdate(sql);
+
         } else {
+
             statement = "INSERT INTO `Minecraft`.`betterlife_players` (`uuid`) VALUES ('"
                     + playerUUID + "')";
-            Bukkit.getLogger().info(ChatColor.LIGHT_PURPLE + statement);
-
-            getStatement().executeUpdate(statement);
+            executeUpdate(statement);
             setValue(row, value, playerUUID);
 
         }
