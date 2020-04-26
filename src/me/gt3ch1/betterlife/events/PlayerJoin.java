113d5427af9683a4348e17d8859e0c03d3b94f46
GT3CH1 <gcpease@gt3ch1.tk>
2020-04-26 10:22:07 -0600

    Added roadboostPerPlayer

--- src/me/gt3ch1/betterlife/events/PlayerJoin.java
+++ src/me/gt3ch1/betterlife/events/PlayerJoin.java
@@ -11,27 +11,34 @@ import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.UUID;
 
+/**
+ * Handles the events for players joining or quitting the game.
+ */
 public class PlayerJoin implements Listener {
     PlayerConfigurationHandler playerConfig  = CommandUtils.getPlayerConfiguration();
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
 
         Player p = e.getPlayer();
         Particle newPlayerParticle = Particle.valueOf(CommandUtils.getMainConfiguration().getCustomConfig().getString("defaultParticle").toUpperCase());
-        
+        UUID playerUUID = p.getUniqueId();
         try{
             playerConfig.getStringValue("trails.enabled", p.getUniqueId());
         }catch (Exception ex){
             playerConfig.setValue("trails.trail", newPlayerParticle.toString(), p.getUniqueId());
             playerConfig.setValue("trails.enabled", false, p.getUniqueId());
         }
-        playerConfig.trailEnabledPerPlayer.put(p.getUniqueId(),playerConfig.getBooleanValue("trails.enabled",p.getUniqueId()));
-        playerConfig.trailPerPlayer.put(p.getUniqueId(),playerConfig.getStringValue("trails.trail",p.getUniqueId()));
+        playerConfig.trailEnabledPerPlayer.put(playerUUID,playerConfig.getBooleanValue("trails.enabled",playerUUID));
+        playerConfig.trailPerPlayer.put(playerUUID,playerConfig.getStringValue("trails.trail",playerUUID));
+        playerConfig.roadboostPerPlayer.put(playerUUID,playerConfig.getBooleanValue("roadboost", playerUUID));
     }
+
     @EventHandler
     public void onPlayerLeave(PlayerQuitEvent e){
         UUID uuid = e.getPlayer().getUniqueId();
         playerConfig.trailEnabledPerPlayer.remove(uuid);
         playerConfig.trailPerPlayer.remove(uuid);
+        playerConfig.roadboostPerPlayer.remove(uuid);
     }
 }
