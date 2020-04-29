01b4c5bdb918838f6b7f7060029b62a298d5eefb
GT3CH1 <gcpease@gt3ch1.tk>
2020-04-28 21:25:28 -0600

    Removed unnecessary imports.

--- src/me/gt3ch1/betterlife/configuration/MainConfigurationHandler.java
+++ src/me/gt3ch1/betterlife/configuration/MainConfigurationHandler.java
@@ -1,23 +1,12 @@
 package me.gt3ch1.betterlife.configuration;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
-
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
-
-import me.gt3ch1.betterlife.Main.Main;
-
 /**
  * This class handles the main configuration of the plugin.
  */
-public class MainConfigurationHandler extends ConfigurationHelper{
+public class MainConfigurationHandler extends ConfigurationHelper {
 
-	public MainConfigurationHandler(){
-		super("config","config",false);
-	}
+    public MainConfigurationHandler() {
+        super("config", "config", false);
+    }
 
 }
