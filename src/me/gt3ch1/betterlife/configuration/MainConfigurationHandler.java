fab71c32756060b013cbc8e82d3572006e7c423b
GT3CH1 <gcpease@gt3ch1.tk>
2020-04-28 19:06:50 -0600

    Made MainConfigurationHandler extend ConfigurationHelper

--- src/me/gt3ch1/betterlife/configuration/MainConfigurationHandler.java
+++ src/me/gt3ch1/betterlife/configuration/MainConfigurationHandler.java
@@ -14,61 +14,10 @@ import me.gt3ch1.betterlife.Main.Main;
 /**
  * This class handles the main configuration of the plugin.
  */
-public class MainConfigurationHandler {
+public class MainConfigurationHandler extends ConfigurationHelper{
 
-	private Main m = Main.m;
-
-	/**
-	 * Plug-in wide configuration handler
-     */
-
-	private static FileConfiguration customConfig = null;
-	private static File customConfigFile = null;
-
-	/**
-	 * Reloads the current state of the configuration file (config.yml)
-	 */
-	public void reloadCustomConfig() {
-		if (customConfigFile == null)
-			customConfigFile = new File(m.getDataFolder(), "config.yml");
-
-		customConfig = YamlConfiguration.loadConfiguration(customConfigFile);
-		Reader defConfigStream = null;
-
-		try {
-			defConfigStream = new InputStreamReader(m.getResource("config.yml"), "UTF8");
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		if (defConfigStream != null) {
-			YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
-			customConfig.setDefaults(defConfig);
-		}
+	public MainConfigurationHandler(){
+		super("config","config",false);
 	}
 
-	/**
-	 * Returns the current instance of the main configuration
-	 * file.
-	 * @return MainConfiguration
-	 */
-	public FileConfiguration getCustomConfig() {
-		if (customConfig == null)
-			reloadCustomConfig();
-
-		return m.getConfig();
-	}
-
-	/**
-	 * Saves the current working state of the main configuration
-	 * to it's file (config.yml)
-	 */
-	public void saveCustomConfig() {
-		if (customConfig == null || customConfigFile == null)
-			return;
-		try {
-			getCustomConfig().save(customConfigFile);
-		} catch (IOException ex) {
-			m.getLogger().severe("Could not save config to " + customConfigFile + "!\n" + ex);
-		}
-	}
 }
