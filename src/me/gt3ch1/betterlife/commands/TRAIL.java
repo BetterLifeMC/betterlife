f4fe8f1dae1f2cea73496062779d25b8ecc3433d
GT3CH1 <gcpease@gt3ch1.tk>
2020-05-03 12:01:25 -0600

    Handle /trail set with missing parameter.

--- src/me/gt3ch1/betterlife/commands/TRAIL.java
+++ src/me/gt3ch1/betterlife/commands/TRAIL.java
@@ -7,7 +7,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class TRAIL extends BetterLifeCommands implements CommandExecutor {
@@ -39,58 +38,67 @@ public class TRAIL extends BetterLifeCommands implements CommandExecutor {
             sendHelpMessage(sender, "trail", helpHash);
             return true;
         }
-        switch (args[0]) {
-            case "list":
-                if (sender instanceof Player) {
-                    Player player = (Player) sender;
-                    sendBannerMessage(player, "&7The currently enabled trails are: ");
-                    for (int i = 0; i < allowedParticles.size(); i++)
-                        if (player.hasPermission("betterlife.trails.particle." + allowedParticles.get(i).toLowerCase()))
-                            sendBannerMessage(player, "&d" + allowedParticles.get(i));
-                } else {
-                    sendBannerMessage(sender, "&7The currently enabled trails are: ");
-                    for (int i = 0; i < allowedParticles.size(); i++)
-                        sendBannerMessage(sender, "&d" + allowedParticles.get(i));
-                }
-                return true;
-            case "toggle":
-                if (sender instanceof Player) {
-                    Player player = (Player) sender;
-                    if (player.hasPermission("betterlife.trail.toggle")) {
-                        boolean trailsEnabled = pch.trailEnabledPerPlayer.get(player.getUniqueId()
-                        );
-                        pch.setValue("trails.enabled", !trailsEnabled, player.getUniqueId());
-                        pch.trailEnabledPerPlayer.replace(player.getUniqueId(), !trailsEnabled);
-                        String toggleState = trailsEnabled ? "&cdisabled" : "&aenabled";
-                        sendBannerMessage(player, "&7Trail " + toggleState + "&7!");
+        switch (args.length) {
+            case 1:
+                switch (args[0]) {
+                    case "list":
+                        if (sender instanceof Player) {
+                            Player player = (Player) sender;
+                            sendBannerMessage(player, "&7The currently enabled trails are: ");
+                            for (int i = 0; i < allowedParticles.size(); i++)
+                                if (player.hasPermission("betterlife.trails.particle." + allowedParticles.get(i).toLowerCase()))
+                                    sendBannerMessage(player, "&d" + allowedParticles.get(i));
+                        } else {
+                            sendBannerMessage(sender, "&7The currently enabled trails are: ");
+                            for (int i = 0; i < allowedParticles.size(); i++)
+                                sendBannerMessage(sender, "&d" + allowedParticles.get(i));
+                        }
                         return true;
-                    }
-                } else {
-                    // TODO: Add console support for attaching a username target as arg[1]
-                    sendBannerMessage(sender, "&4You must be a player to use this command!");
-                    return true;
+                    case "toggle":
+                        if (sender instanceof Player) {
+                            Player player = (Player) sender;
+                            if (player.hasPermission("betterlife.trail.toggle")) {
+                                boolean trailsEnabled = pch.trailEnabledPerPlayer.get(player.getUniqueId()
+                                );
+                                pch.setValue("trails.enabled", !trailsEnabled, player.getUniqueId());
+                                pch.trailEnabledPerPlayer.replace(player.getUniqueId(), !trailsEnabled);
+                                String toggleState = trailsEnabled ? "&cdisabled" : "&aenabled";
+                                sendBannerMessage(player, "&7Trail " + toggleState + "&7!");
+                                return true;
+                            }
+                        } else {
+                            // TODO: Add console support for attaching a username target as arg[1]
+                            sendBannerMessage(sender, "&4You must be a player to use this command!");
+                            return true;
+                        }
+                        return true;
+                    case "trail":
+                        sendHelpMessage(sender, "trail", helpHash);
+                        return true;
+                    default:
+                        return false;
                 }
-                return true;
-            case "set":
-                if (sender instanceof Player) {
-                    Player player = (Player) sender;
-                    if (allowedParticles.contains(args[1].toUpperCase()) && (player.hasPermission("betterlife.trail.particle." + args[1].toLowerCase()) || player.hasPermission("betterlife.trail.particle.*"))) {
-                        pch.setValue("trails.trail", args[1].toUpperCase(), player.getUniqueId());
-                        sendBannerMessage(player, "&7Your trail is now set to: &6" + args[1].toUpperCase());
-                        pch.trailPerPlayer.replace(player.getUniqueId(), args[1].toUpperCase());
+            case 2:
+                if (args[0].equalsIgnoreCase("set"))
+                    if (sender instanceof Player) {
+                        Player player = (Player) sender;
+                        if (allowedParticles.contains(args[1].toUpperCase()) && (player.hasPermission("betterlife.trail.particle." + args[1].toLowerCase()) || player.hasPermission("betterlife.trail.particle.*"))) {
+                            pch.setValue("trails.trail", args[1].toUpperCase(), player.getUniqueId());
+                            sendBannerMessage(player, "&7Your trail is now set to: &6" + args[1].toUpperCase());
+                            pch.trailPerPlayer.replace(player.getUniqueId(), args[1].toUpperCase());
 
-                    } else if (!player.hasPermission("betterlife.trail.particle." + args[1].toLowerCase()))
-                        sendBannerMessage(player, "&4You need permission to use that trail!");
-                    else if (!allowedParticles.contains(args[1].toUpperCase()))
-                        sendBannerMessage(player, "&4That particle is disabled on this server! Try /trail list to see the list of available trails.");
-                } else {
-                    // TODO: Add console support for attaching a username target as arg[2]
-                    sendBannerMessage(sender, "&4You must be a player to use this command!");
-                }
+                        } else if (!player.hasPermission("betterlife.trail.particle." + args[1].toLowerCase()))
+                            sendBannerMessage(player, "&4You need permission to use that trail!");
+                        else if (!allowedParticles.contains(args[1].toUpperCase()))
+                            sendBannerMessage(player, "&4That particle is disabled on this server! Try /trail list to see the list of available trails.");
+                    } else {
+                        // TODO: Add console support for attaching a username target as arg[2]
+                        sendBannerMessage(sender, "&4You must be a player to use this command!");
+                    }
                 return true;
             default:
                 sendHelpMessage(sender, "trail", helpHash);
-                return true;
+                return false;
         }
     }
 }
