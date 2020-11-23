ac4af46e5753952c0f9c018b430ce7666f34e6ce
GT3CH1 <gavinpease@gmail.com>
2020-11-23 11:46:59 -0700

    Renabled caching.

--- src/main/java/me/gt3ch1/betterlife/data/BL_PLAYER.java
+++ src/main/java/me/gt3ch1/betterlife/data/BL_PLAYER.java
@@ -2,14 +2,24 @@ package me.gt3ch1.betterlife.data;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.UUID;
 import me.gt3ch1.betterlife.Main.Main;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 
 public class BL_PLAYER {
 
     private final Sql sql = Main.sql;
     private ResultSet rs;
+
+    //TODO: Re-enable caching
+    public HashMap<UUID, Boolean> trailEnabledPerPlayer = new HashMap<>();
+    public HashMap<UUID, String> trailPerPlayer = new HashMap<>();
+    public HashMap<UUID, Boolean> roadboostPerPlayer = new HashMap<>();
+    public HashMap<UUID, Location> antiGriefLocation1PerPlayer = new HashMap<>();
+    public HashMap<UUID, Location> antiGriefLocation2PerPlayer = new HashMap<>();
+
 
     public boolean getPlayerToggle(UUID playerUUID, String toggle) {
         String query =
