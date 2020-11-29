c705c4e55ed56966edd965c079a5e40bd6662d22
GT3CH1 <gavinpease@gmail.com>
2020-11-29 10:39:24 -0700

    Sat up another enum for sql type to insert into BL_PLAYER sql table

--- src/main/java/me/gt3ch1/betterlife/data/BL_PLAYER_ENUM.java
+++ src/main/java/me/gt3ch1/betterlife/data/BL_PLAYER_ENUM.java
@@ -9,13 +9,21 @@ package me.gt3ch1.betterlife.data;
  */
 public enum BL_PLAYER_ENUM {
 
-    TRAIL_ENABLED_PER_PLAYER("TrailToggle","BL_PLAYER"),
-    TRAIL_PER_PLAYER("Trail","BL_PLAYER"),
-    ROADBOOST_PER_PLAYER("RoadBoostToggle","BL_PLAYER"),
+    TRAIL_ENABLED_PER_PLAYER("TrailToggle","BL_PLAYER","BOOL"),
+    TRAIL_PER_PLAYER("Trail","BL_PLAYER","NVCHAR(30)"),
+    ROADBOOST_PER_PLAYER("RoadBoostToggle","BL_PLAYER","BOOL"),
+    MUTE_PER_PLAYER("Mute","BL_PLAYER","BOOL"),
     HOME_PER_PLAYER("Home","BL_HOME");
 
     private String column;
     private String table;
+    private String sqlType;
+
+    BL_PLAYER_ENUM(String column, String table, String sqlType){
+        this.column = column;
+        this.table = table;
+        this.sqlType = sqlType;
+    }
 
     BL_PLAYER_ENUM(String column, String table){
         this.column = column;
@@ -37,4 +45,10 @@ public enum BL_PLAYER_ENUM {
     public String getTable(){
         return table;
     }
+
+    /**
+     * Gets the SQL data type.
+     * @return SQL data type.
+     */
+    public String getSqlType(){ return sqlType; }
 }
