064385255dd4fc11acacc4febfa608e2ba627040
GT3CH1 <gavinpease@gmail.com>
2020-11-28 20:11:40 -0700

    Added missing javadoc for BL_PLAYER_ENUM

--- src/main/java/me/gt3ch1/betterlife/data/BL_PLAYER_ENUM.java
+++ src/main/java/me/gt3ch1/betterlife/data/BL_PLAYER_ENUM.java
@@ -9,32 +9,40 @@ package me.gt3ch1.betterlife.data;
  */
 public enum BL_PLAYER_ENUM {
 
-    TRAIL_ENABLED_PER_PLAYER("TrailToggle","BL_PLAYER"),
-    TRAIL_PER_PLAYER("Trail","BL_PLAYER"),
-    ROADBOOST_PER_PLAYER("RoadBoostToggle","BL_PLAYER"),
-    HOME_PER_PLAYER("Home","BL_HOME");
+    TRAIL_ENABLED_PER_PLAYER("TrailToggle", "BL_PLAYER"),
+    TRAIL_PER_PLAYER("Trail", "BL_PLAYER"),
+    ROADBOOST_PER_PLAYER("RoadBoostToggle", "BL_PLAYER"),
+    HOME_PER_PLAYER("Home", "BL_HOME");
 
     private String column;
     private String table;
 
-    BL_PLAYER_ENUM(String column, String table){
+    /**
+     * Creates a BetterLife player type.
+     *
+     * @param column Column in the given SQL table.
+     * @param table  Table that contains column.
+     */
+    BL_PLAYER_ENUM(String column, String table) {
         this.column = column;
         this.table = table;
     }
 
     /**
      * Gets the column that corresponds to the SQL table.
+     *
      * @return SQL Column.
      */
-    public String getColumn(){
+    public String getColumn() {
         return column;
     }
 
     /**
      * Gets the table name.
-     * @return  SQL table name.
+     *
+     * @return SQL table name.
      */
-    public String getTable(){
+    public String getTable() {
         return table;
     }
 }
