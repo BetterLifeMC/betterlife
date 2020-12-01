4a39d1cf9776919138f419605bf438fa26d2ffba
GT3CH1 <gavinpease@gmail.com>
2020-12-01 13:42:09 -0700

    Cleaned up PlayerJoin

--- src/main/java/me/gt3ch1/betterlife/events/PlayerJoin.java
+++ src/main/java/me/gt3ch1/betterlife/events/PlayerJoin.java
@@ -37,41 +37,4 @@ public class PlayerJoin implements Listener {
 
     }
 
-    /**
-     * Set's up the cache for all player data
-     *
-     * @param playerUUID Player UUID
-     */
-    public static void setupPlayerConfig(UUID playerUUID) {
-        playerGetter.trailEnabledPerPlayer.put(playerUUID, playerGetter.getPlayerToggle(playerUUID, BL_PLAYER_ENUM.TRAIL_ENABLED_PER_PLAYER));
-        playerGetter.trailPerPlayer.put(playerUUID, playerGetter.getPlayerString(playerUUID, BL_PLAYER_ENUM.TRAIL_PER_PLAYER));
-        playerGetter.roadboostPerPlayer.put(playerUUID, playerGetter.getPlayerToggle(playerUUID, BL_PLAYER_ENUM.ROADBOOST_PER_PLAYER));
-    }
-
-    /**
-     * Clears the player configuration cache
-     */
-    public static void clearPlayerConfigs() {
-        try {
-            playerGetter.trailEnabledPerPlayer.clear();
-            playerGetter.trailPerPlayer.clear();
-            playerGetter.roadboostPerPlayer.clear();
-        } catch (Exception e) {
-
-        }
-    }
-
-    /**
-     * Clears the cache for the player
-     *
-     * @param uuid Player UUID
-     */
-    public static void clearPlayerConfigs(UUID uuid) {
-        try {
-            playerGetter.trailEnabledPerPlayer.remove(uuid);
-            playerGetter.trailPerPlayer.remove(uuid);
-            playerGetter.roadboostPerPlayer.remove(uuid);
-        } catch (Exception e) {
-        }
-    }
 }
