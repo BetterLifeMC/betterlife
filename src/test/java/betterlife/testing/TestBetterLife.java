974c63b79e8cf5cb37470e5a8e0bbde0849a6660
GT3CH1 <gavinpease@gmail.com>
2020-12-15 18:34:54 -0700

    renamed testPlayer ; added teleport test case

--- src/test/java/betterlife/testing/TestBetterLife.java
+++ src/test/java/betterlife/testing/TestBetterLife.java
@@ -18,25 +18,28 @@ import static org.junit.Assert.*;
 public class TestBetterLife {
     private ServerMock server;
     private Main plugin;
-    private UUID mockUUID = UUID.fromString("ed275ae9-e765-408b-b056-3d5a5d540626");
-    private PlayerMock player;
+    private UUID playerOneUUID = UUID.fromString("ed275ae9-e765-408b-b056-3d5a5d540626");
+    private UUID playerTwoUUID = UUID.fromString("ed275ae9-e765-408b-b056-3d5a5d540627");
+    private PlayerMock playerOne;
+    private PlayerMock playerTwo;
 
     @Before
     public void setUp() {
         server = MockBukkit.mock();
         plugin = (Main) MockBukkit.load(Main.class);
         plugin.onLoad();
-        player = new PlayerMock(server, "testplayer", mockUUID);
-        server.addPlayer(player);
-        player.setOp(true);
+        playerOne = new PlayerMock(server, "playerone", playerOneUUID);
+        playerTwo = new PlayerMock(server, "playertwo", playerTwoUUID);
+        server.addPlayer(playerOne);
+        playerOne.setOp(true);
     }
 
     @Test
     public void testRoadBoost() {
         BL_PLAYER_ENUM type = BL_PLAYER_ENUM.ROADBOOST_PER_PLAYER;
-        boolean previousRoadBoostState = plugin.getBlPlayer().getPlayerToggle(mockUUID, type);
-        assertTrue(player.performCommand("roadboost"));
-        boolean currentRoadboostState = plugin.getBlPlayer().getPlayerToggle(mockUUID, type);
+        boolean previousRoadBoostState = plugin.getBlPlayer().getPlayerToggle(playerOneUUID, type);
+        assertTrue(playerOne.performCommand("roadboost"));
+        boolean currentRoadboostState = plugin.getBlPlayer().getPlayerToggle(playerOneUUID, type);
         assertTrue(previousRoadBoostState != currentRoadboostState);
     }
 
@@ -47,55 +50,55 @@ public class TestBetterLife {
 
     @Test
     public void testPlayerMute(){
-        player.performCommand("mute testplayer");
-        assertTrue(plugin.getBlPlayer().getPlayerToggle(mockUUID,BL_PLAYER_ENUM.MUTE_PER_PLAYER));
+        playerOne.performCommand("mute testplayer");
+        assertTrue(plugin.getBlPlayer().getPlayerToggle(playerOneUUID,BL_PLAYER_ENUM.MUTE_PER_PLAYER));
     }
 
     @Test
     public void testPlayerUnmute(){
-        player.performCommand("unmute testplayer");
-        assertFalse(plugin.getBlPlayer().getPlayerToggle(mockUUID,BL_PLAYER_ENUM.MUTE_PER_PLAYER));
+        playerOne.performCommand("unmute testplayer");
+        assertFalse(plugin.getBlPlayer().getPlayerToggle(playerOneUUID,BL_PLAYER_ENUM.MUTE_PER_PLAYER));
     }
 
     @Test
     public void testTrailToggle(){
         BL_PLAYER_ENUM type = BL_PLAYER_ENUM.TRAIL_ENABLED_PER_PLAYER;
-        boolean previousTrailState = plugin.getBlPlayer().getPlayerToggle(mockUUID, type);
-        player.performCommand("trail toggle");
-        boolean currentTrailState = plugin.getBlPlayer().getPlayerToggle(mockUUID, type);
+        boolean previousTrailState = plugin.getBlPlayer().getPlayerToggle(playerOneUUID, type);
+        playerOne.performCommand("trail toggle");
+        boolean currentTrailState = plugin.getBlPlayer().getPlayerToggle(playerOneUUID, type);
         assertTrue(previousTrailState != currentTrailState);
     }
 
     @Test
     public void setTrailToCrit(){
         BL_PLAYER_ENUM type = BL_PLAYER_ENUM.TRAIL_PER_PLAYER;
-        player.performCommand("trail set crit");
-        String currentTrail = plugin.getBlPlayer().getPlayerString(mockUUID,type);
+        playerOne.performCommand("trail set crit");
+        String currentTrail = plugin.getBlPlayer().getPlayerString(playerOneUUID,type);
         assertEquals("CRIT",currentTrail);
     }
 
     @Test
     public void setTrailToNote(){
         BL_PLAYER_ENUM type = BL_PLAYER_ENUM.TRAIL_PER_PLAYER;
-        player.performCommand("trail set note");
-        String currentTrail = plugin.getBlPlayer().getPlayerString(mockUUID,type);
+        playerOne.performCommand("trail set note");
+        String currentTrail = plugin.getBlPlayer().getPlayerString(playerOneUUID,type);
         assertEquals("NOTE",currentTrail);
     }
 
     @Test
     public void setTrailToCloud(){
         BL_PLAYER_ENUM type = BL_PLAYER_ENUM.TRAIL_PER_PLAYER;
-        player.performCommand("trail set cloud");
-        String currentTrail = plugin.getBlPlayer().getPlayerString(mockUUID,type);
+        playerOne.performCommand("trail set cloud");
+        String currentTrail = plugin.getBlPlayer().getPlayerString(playerOneUUID,type);
         assertEquals("CLOUD",currentTrail);
     }
 
     @Test
     public void setTrailToInvalid(){
         BL_PLAYER_ENUM type = BL_PLAYER_ENUM.TRAIL_PER_PLAYER;
-        String previousTrail = plugin.getBlPlayer().getPlayerString(mockUUID,type);
-        player.performCommand("trail set dinkleberg");
-        String currentTrail = plugin.getBlPlayer().getPlayerString(mockUUID,type);
+        String previousTrail = plugin.getBlPlayer().getPlayerString(playerOneUUID,type);
+        playerOne.performCommand("trail set dinkleberg");
+        String currentTrail = plugin.getBlPlayer().getPlayerString(playerOneUUID,type);
         assertEquals(previousTrail,currentTrail);
     }
 
@@ -104,42 +107,42 @@ public class TestBetterLife {
         World w = server.getWorld("world");
         w.setStorm(false);
         assertFalse(w.hasStorm());
-        player.performCommand("toggledownfall");
+        playerOne.performCommand("toggledownfall");
         assertTrue(w.hasStorm());
     }
 
     @Test
     public void createWarp(){
-        player.performCommand("warp set test_warp");
+        playerOne.performCommand("warp set test_warp");
         assertTrue(plugin.getBlWarps().getWarps().size() > 0);
-        player.performCommand("warp del test_warp");
+        playerOne.performCommand("warp del test_warp");
     }
 
     @Test
     public void deleteWarp(){
-        player.performCommand("warp set test_warp");
+        playerOne.performCommand("warp set test_warp");
         int previousSize = plugin.getBlWarps().getWarps().size();
-        player.performCommand("warp del test_warp");
+        playerOne.performCommand("warp del test_warp");
         assertEquals(previousSize-1,plugin.getBlWarps().getWarps().size());
     }
 
     @Test
     public void createMultipleWarps(){
         int previousSize = plugin.getBlWarps().getWarps().size();
-        player.performCommand("warp set test_warp_1");
-        player.performCommand("warp set test_warp_2");
+        playerOne.performCommand("warp set test_warp_1");
+        playerOne.performCommand("warp set test_warp_2");
         assertEquals(previousSize+2,plugin.getBlWarps().getWarps().size());
-        player.performCommand("warp del test_warp_1");
-        player.performCommand("warp del test_warp_2");
+        playerOne.performCommand("warp del test_warp_1");
+        playerOne.performCommand("warp del test_warp_2");
     }
 
     @Test
     public void deleteMultipleWarps(){
         int previousSize = plugin.getBlWarps().getWarps().size();
-        player.performCommand("warp set test_warp_1");
-        player.performCommand("warp set test_warp_2");
-        player.performCommand("warp del test_warp_1");
-        player.performCommand("warp del test_warp_2");
+        playerOne.performCommand("warp set test_warp_1");
+        playerOne.performCommand("warp set test_warp_2");
+        playerOne.performCommand("warp del test_warp_1");
+        playerOne.performCommand("warp del test_warp_2");
         assertEquals(previousSize,plugin.getBlWarps().getWarps().size());
 
     }
@@ -147,75 +150,84 @@ public class TestBetterLife {
     @Test
     public void testWarpTeleport(){
         Location testLocation = new Location(server.getWorld("world"),100,100,100);
-        player.teleport(testLocation);
-        player.performCommand("warp set test_warp");
-        player.teleport(new Location(server.getWorld("world"),0,5,0));
-        player.performCommand("warp test_warp");
-        assertEquals(testLocation,player.getLocation());
-        player.performCommand("warp del test_warp");
+        playerOne.teleport(testLocation);
+        playerOne.performCommand("warp set test_warp");
+        playerOne.teleport(new Location(server.getWorld("world"),0,5,0));
+        playerOne.performCommand("warp test_warp");
+        assertEquals(testLocation, playerOne.getLocation());
+        playerOne.performCommand("warp del test_warp");
     }
 
     @Test
     public void testWarpSetLocation(){
         Location testLocation = new Location(server.getWorld("world"),100,100,100);
-        player.teleport(testLocation);
-        player.performCommand("warp set test_warp");
-        player.teleport(new Location(server.getWorld("world"),0,5,0));
+        playerOne.teleport(testLocation);
+        playerOne.performCommand("warp set test_warp");
+        playerOne.teleport(new Location(server.getWorld("world"),0,5,0));
         assertEquals(testLocation,plugin.getBlWarps().getWarps().get("test_warp"));
-        player.performCommand("warp del test_warp");
+        playerOne.performCommand("warp del test_warp");
     }
 
     @Test
     public void createHome(){
-        player.performCommand("home set test_home");
-        assertEquals(1,plugin.getBlHomes().getHomes(mockUUID).size());
-        player.performCommand("home del test_home");
+        playerOne.performCommand("home set test_home");
+        assertEquals(1,plugin.getBlHomes().getHomes(playerOneUUID).size());
+        playerOne.performCommand("home del test_home");
     }
 
     @Test
     public void deleteHome(){
-        player.performCommand("home set test_home");
-        player.performCommand("home del test_home");
-        assertEquals(0,plugin.getBlHomes().getHomes(mockUUID).size());
+        playerOne.performCommand("home set test_home");
+        playerOne.performCommand("home del test_home");
+        assertEquals(0,plugin.getBlHomes().getHomes(playerOneUUID).size());
     }
 
     @Test
     public void createMultipleHomes(){
-        player.performCommand("home set test_home_1");
-        player.performCommand("home set test_home_2");
-        assertEquals(2,plugin.getBlHomes().getHomes(mockUUID).size());
-        player.performCommand("home del test_home_1");
-        player.performCommand("home del test_home_2");
+        playerOne.performCommand("home set test_home_1");
+        playerOne.performCommand("home set test_home_2");
+        assertEquals(2,plugin.getBlHomes().getHomes(playerOneUUID).size());
+        playerOne.performCommand("home del test_home_1");
+        playerOne.performCommand("home del test_home_2");
     }
 
     @Test
     public void deleteMultipleHomes(){
-        player.performCommand("home set test_home_1");
-        player.performCommand("home set test_home_2");
-        player.performCommand("home del test_home_1");
-        player.performCommand("home del test_home_2");
-        assertEquals(0,plugin.getBlHomes().getHomes(mockUUID).size());
+        playerOne.performCommand("home set test_home_1");
+        playerOne.performCommand("home set test_home_2");
+        playerOne.performCommand("home del test_home_1");
+        playerOne.performCommand("home del test_home_2");
+        assertEquals(0,plugin.getBlHomes().getHomes(playerOneUUID).size());
     }
 
     @Test
     public void testHomeSetLocation(){
         Location testLocation = new Location(server.getWorld("world"),100,100,100);
-        player.teleport(testLocation);
-        player.performCommand("home set test_home");
-        player.teleport(new Location(server.getWorld("world"),0,5,0));
-        assertEquals(testLocation,plugin.getBlHomes().getHomes(mockUUID).get("test_home"));
-        player.performCommand("home del test_home");
+        playerOne.teleport(testLocation);
+        playerOne.performCommand("home set test_home");
+        playerOne.teleport(new Location(server.getWorld("world"),0,5,0));
+        assertEquals(testLocation,plugin.getBlHomes().getHomes(playerOneUUID).get("test_home"));
+        playerOne.performCommand("home del test_home");
     }
 
     @Test
     public void testHomeTeleport(){
         Location testLocation = new Location(server.getWorld("world"),100,100,100);
-        player.teleport(testLocation);
-        player.performCommand("home set test_home");
-        player.teleport(new Location(server.getWorld("world"),0,5,0));
-        player.performCommand("home test_home");
-        assertEquals(testLocation,player.getLocation());
-        player.performCommand("home del test_home");
+        playerOne.teleport(testLocation);
+        playerOne.performCommand("home set test_home");
+        playerOne.teleport(new Location(server.getWorld("world"),0,5,0));
+        playerOne.performCommand("home test_home");
+        assertEquals(testLocation, playerOne.getLocation());
+        playerOne.performCommand("home del test_home");
+    }
+
+    @Test
+    public void testPlayerTeleport(){
+        Location testLocation = new Location(server.getWorld("world"),100,100,100);
+        playerTwo.teleport(testLocation);
+        playerOne.performCommand("teleport playertwo");
+        assertEquals(testLocation, playerOne.getLocation());
+        playerOne.teleport(new Location(server.getWorld("world"),0,5,0));
     }
 
     @After
