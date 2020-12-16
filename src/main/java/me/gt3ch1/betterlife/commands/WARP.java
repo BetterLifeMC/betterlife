020e01abbfaa2319916484a3eea27962cc278884
GT3CH1 <gavinpease@gmail.com>
2020-12-16 08:54:11 -0700

    Add permission checks for warp

--- src/main/java/me/gt3ch1/betterlife/commands/WARP.java
+++ src/main/java/me/gt3ch1/betterlife/commands/WARP.java
@@ -29,6 +29,7 @@ public class WARP extends BetterLifeCommands implements CommandExecutor {
 
     /**
      * Runs the /warp command
+     *
      * @param sender  Sender of the command.
      * @param c       The command itself.
      * @param command The string version of the command.
@@ -50,20 +51,20 @@ public class WARP extends BetterLifeCommands implements CommandExecutor {
                 if (args[0].equalsIgnoreCase("list")) {
                     sendMessage(player, ChatColor.LIGHT_PURPLE + "Available warps:", true);
                     for (String names : Main.bl_warp.getWarps().keySet())
-                        if (sender.hasPermission("betterlife.warps." + names.toLowerCase()))
+                        if (sender.hasPermission("betterlife.warps." + names.toLowerCase()) || sender.hasPermission("betterlife.warps.*"))
                             sendMessage(player, ChatColor.AQUA + names, false);
 
                     return true;
                 }
                 if (warps.getWarps().size() > 0 && warps.getWarps().containsKey(args[0])) {
-                    if (sender.hasPermission("betterlife.warps." + args[0])) {
+                    if (sender.hasPermission("betterlife.warps." + args[0]) || sender.hasPermission("betterlife.warps.*")) {
                         teleportHelper.teleportPlayer(player, warps.getWarps().get(args[0]), args[0]);
                         return true;
                     }
                 }
             case 2:
                 if (args[0].equalsIgnoreCase("set")) {
-                    if (sender.hasPermission("betterlife.warps.set")) {
+                    if (sender.hasPermission("betterlife.warps.set") || sender.hasPermission("betterlife.warps.*")) {
                         Main.bl_warp.addWarp(player, args[1]);
                         sendMessage(sender, ChatColor.AQUA + "Warp " + ChatColor.YELLOW + args[1] +
                                 ChatColor.AQUA + " sat to your location", true);
@@ -71,7 +72,7 @@ public class WARP extends BetterLifeCommands implements CommandExecutor {
                     }
                 }
                 if (args[0].equalsIgnoreCase("del")) {
-                    if (sender.hasPermission("betterlife.warps.del")) {
+                    if (sender.hasPermission("betterlife.warps.del") || sender.hasPermission("betterlife.warps.*")) {
                         Main.bl_warp.delWarp(args[1]);
                         sendMessage(sender, ChatColor.AQUA + "Warp " + ChatColor.YELLOW + args[1] +
                                 ChatColor.AQUA + " has been deleted.", true);
