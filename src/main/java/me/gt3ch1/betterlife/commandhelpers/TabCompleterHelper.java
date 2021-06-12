9cbb0b1023b7f13d69a317a44fcd1b3b92315646
Starmism <iamstarmism@gmail.com>
2021-06-12 02:26:49 -0600

    Cleanup!

--- src/main/java/me/gt3ch1/betterlife/commandhelpers/TabCompleterHelper.java
+++ src/main/java/me/gt3ch1/betterlife/commandhelpers/TabCompleterHelper.java
@@ -8,6 +8,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 
@@ -25,7 +26,7 @@ public class TabCompleterHelper implements TabCompleter {
      * When we can tab complete commands. NOTE: Must be enabled in CommandUtils!
      */
     @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, String[] args) {
         subCommands.clear();
 
         if (sender instanceof Player player) {
