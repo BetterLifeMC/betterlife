53058d3d54e7cf15a8f8c86e233af16abd401e25
Starmism <iamstarmism@gmail.com>
2021-06-12 02:12:10 -0600

    Cleanup!

--- src/main/java/me/gt3ch1/betterlife/commands/TRAIL.java
+++ src/main/java/me/gt3ch1/betterlife/commands/TRAIL.java
@@ -1,5 +1,6 @@
 package me.gt3ch1.betterlife.commands;
 
+import java.util.HashSet;
 import java.util.UUID;
 import me.gt3ch1.betterlife.Main.BetterLife;
 import me.gt3ch1.betterlife.commandhelpers.BetterLifeCommands;
@@ -10,8 +11,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
-import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
 public class TRAIL extends BetterLifeCommands implements CommandExecutor {
 
@@ -43,25 +43,26 @@ public class TRAIL extends BetterLifeCommands implements CommandExecutor {
      * @return True if the command was successful.
      */
     @Override
-    public boolean onCommand(CommandSender sender, Command c, String command, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command c, @NotNull String command, String[] args) {
 
-        List<String> allowedParticles = CommandUtils.partch.getCustomConfig()
-            .getStringList("particle");
+        var allowedParticles = new HashSet<>(CommandUtils.partch.getCustomConfig().getStringList("particle"));
+
         if (args.length == 0) {
             sendHelpMessage(sender, "trail", helpHash);
             return true;
         }
+
         switch (args.length) {
-            case 1:
+            case 1 -> {
                 switch (args[0]) {
-                    case "list":
-                        if (sender instanceof Player) {
-                            Player player = (Player) sender;
+                    case "list" -> {
+                        if (sender instanceof Player player) {
                             sendMessage(player, "&7The currently enabled trails are: ", true);
+
                             for (String allowedParticle : allowedParticles) {
                                 if (player.hasPermission(
-                                    "betterlife.trails.particle." + allowedParticle.toLowerCase())
-                                    || player.hasPermission("betterlife.trails.particle.*")) {
+                                        "betterlife.trails.particle." + allowedParticle.toLowerCase())
+                                        || player.hasPermission("betterlife.trails.particle.*")) {
                                     sendMessage(player, "&d" + allowedParticle, true);
                                 }
                             }
@@ -72,38 +73,39 @@ public class TRAIL extends BetterLifeCommands implements CommandExecutor {
                             }
                         }
                         return true;
-                    case "toggle":
-                        if (sender instanceof Player) {
-                            Player player = (Player) sender;
+                    }
+                    case "toggle" -> {
+                        if (sender instanceof Player player) {
                             UUID playerUUID = player.getUniqueId();
                             if (player.hasPermission("betterlife.trail.toggle")) {
                                 boolean trailsEnabled = playerGetter.getPlayerToggle(playerUUID, BL_PLAYER_ENUM.TRAIL_ENABLED_PER_PLAYER);
                                 playerGetter.setPlayerToggle(playerUUID, BL_PLAYER_ENUM.TRAIL_ENABLED_PER_PLAYER);
                                 String toggleState = trailsEnabled ? "&cdisabled" : "&aenabled";
                                 sendMessage(player, "&7Trail " + toggleState + "&7!", true);
-                                return true;
                             }
                         } else {
                             // TODO: Add console support for attaching a username target as arg[1]
                             sendMessage(sender, "&4You must be a player to use this command!", true);
-                            return true;
                         }
                         return true;
-                    case "trail":
+                    }
+                    case "trail" -> {
                         sendHelpMessage(sender, "trail", helpHash);
                         return true;
-                    default:
+                    }
+                    default -> {
                         return false;
+                    }
                 }
-            case 2:
+            }
+            case 2 -> {
                 if (args[0].equalsIgnoreCase("set")) {
-                    if (sender instanceof Player) {
-                        Player player = (Player) sender;
+                    if (sender instanceof Player player) {
                         UUID playerUUID = player.getUniqueId();
 
                         if (allowedParticles.contains(args[1].toUpperCase()) && (
-                            player.hasPermission("betterlife.trail.particle." + args[1].toLowerCase())
-                                || player.hasPermission("betterlife.trail.particle.*"))) {
+                                player.hasPermission("betterlife.trail.particle." + args[1].toLowerCase())
+                                        || player.hasPermission("betterlife.trail.particle.*"))) {
 
                             playerGetter.setPlayerString(playerUUID, BL_PLAYER_ENUM.TRAIL_PER_PLAYER, args[1].toUpperCase());
                             sendMessage(player, "&7Your trail is now set to: &6" + args[1].toUpperCase(), true);
@@ -112,7 +114,7 @@ public class TRAIL extends BetterLifeCommands implements CommandExecutor {
                             sendMessage(player, "&4You need permission to use that trail!", true);
                         } else if (!allowedParticles.contains(args[1].toUpperCase())) {
                             sendMessage(player, "&4That particle is disabled on this server! Try /trail list to see the list of available trails.",
-                                true);
+                                    true);
                         }
                     } else {
                         // TODO: Add console support for attaching a username target as arg[2]
@@ -120,9 +122,11 @@ public class TRAIL extends BetterLifeCommands implements CommandExecutor {
                     }
                 }
                 return true;
-            default:
+            }
+            default -> {
                 sendHelpMessage(sender, "trail", helpHash);
-                return false;
+                return true;
+            }
         }
     }
 }
