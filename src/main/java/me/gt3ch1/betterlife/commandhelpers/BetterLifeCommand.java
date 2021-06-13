29a9fd22243ed47cc54f311da0877becc3d0bc76
Starmism <iamstarmism@gmail.com>
2021-06-12 20:20:16 -0600

    Added superclass documentation for every command

--- src/main/java/me/gt3ch1/betterlife/commandhelpers/BetterLifeCommand.java
+++ src/main/java/me/gt3ch1/betterlife/commandhelpers/BetterLifeCommand.java
@@ -5,7 +5,6 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-
 /**
  * @author Starmism
  */
@@ -17,13 +16,28 @@ public interface BetterLifeCommand {
     String getPermission();
 
     /**
-     * Gives you the list of help strings for the subcommands.
+     * @return The list of help strings for the subcommands
      */
     List<String> getHelpList();
 
+    /**
+     * @return The command description
+     */
     String getDescription();
 
+    /**
+     * Called when the command is run by a either a player or the console
+     * @param sender Player or console who sent the command
+     * @param args The list of arguments ran with the command
+     * @return Essentially always true, since we never want to print the usage message from the plugin.yml
+     */
     boolean onCommand(CommandSender sender, String[] args);
 
+    /**
+     * Called upon every letter that is typed as a command to give information about possible options
+     * @param sender Player or console who is typing the command
+     * @param args The list of arguments (so far)
+     * @return A List containing all the possible options for the next argument to be typed
+     */
     List<String> tabComplete(@NotNull CommandSender sender, @NotNull final String[] args);
 }
